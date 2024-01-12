0ffc70105ff02b04266bd74fbcc6d827c84c2345
alikhokha807 <ali.khokha2000@gmail.com>
2024-01-11 16:41:52 -0800

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stddef.h> // for size_t
+
+// Define the structure for a doubly linked list node
+typedef struct dlistint_s {
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+// Function to get the number of elements in a doubly linked list
+size_t dlistint_len(const dlistint_t *h) {
+    size_t node_count = 0;
+
+    // Traverse the list and count each element
+    while (h != NULL) {
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
+}
+
+// Example usage in a main function
+int main(void) {
+    // Create a sample doubly linked list
+    dlistint_t node1 = {1, NULL, NULL};
+    dlistint_t node2 = {2, NULL, NULL};
+    dlistint_t node3 = {3, NULL, NULL};
+
+    // Link the nodes
+    node1.next = &node2;
+    node2.prev = &node1;
+    node2.next = &node3;
+    node3.prev = &node2;
+
+    // Get the length of the doubly linked list
+    size_t count = dlistint_len(&node1);
+
+    printf("Number of nodes: %zu\n", count);
+
+    return 0;
+}
+
