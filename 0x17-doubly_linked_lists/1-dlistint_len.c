38f4a6bb374ca2ee4bf63511ca6d8e8e0bf56c6f
alikhokha807 <ali.khokha2000@gmail.com>
2024-01-11 16:43:52 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,44 +1,26 @@
-#include <stdio.h>
-#include <stddef.h> // for size_t
+#include "lists.h"
 
-// Define the structure for a doubly linked list node
-typedef struct dlistint_s {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
+/**
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of elements in the dlistint_t list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    /* Variable to store the number of elements in the list. */
+    size_t nodes = 0;
 
-// Function to get the number of elements in a doubly linked list
-size_t dlistint_len(const dlistint_t *h) {
-    size_t node_count = 0;
+    while (h)
+    {
+        /* Increment the element count. */
+        nodes++;
 
-    // Traverse the list and count each element
-    while (h != NULL) {
+        /* Move to the next element. */
         h = h->next;
-        node_count++;
     }
 
-    return node_count;
-}
-
-// Example usage in a main function
-int main(void) {
-    // Create a sample doubly linked list
-    dlistint_t node1 = {1, NULL, NULL};
-    dlistint_t node2 = {2, NULL, NULL};
-    dlistint_t node3 = {3, NULL, NULL};
-
-    // Link the nodes
-    node1.next = &node2;
-    node2.prev = &node1;
-    node2.next = &node3;
-    node3.prev = &node2;
-
-    // Get the length of the doubly linked list
-    size_t count = dlistint_len(&node1);
-
-    printf("Number of nodes: %zu\n", count);
-
-    return 0;
+    /* Return the total number of elements in the list. */
+    return (nodes);
 }
 
